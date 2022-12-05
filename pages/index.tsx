c66cf5e016e446db17db24b447aaf1bd58a9abdb
Danny Gelman <dannygelman1@gmail.com>
2022-12-04 21:59:53 -0500

    installing tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="bg-green-500 font-bold text-2xl">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
