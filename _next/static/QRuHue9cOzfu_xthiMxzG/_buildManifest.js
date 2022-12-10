98306e2791482f4a01a56b6a4d6e24177f239844
dannygelman1 <45411340+dannygelman1@users.noreply.github.com>
2022-12-10 20:19:58 +0000

    Deploying to gh-pages from @ dannygelman1/Wings@9241e9b3b9bc757d8538e05955e261e216650989 🚀

--- _next/static/QRuHue9cOzfu_xthiMxzG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-2fe8122be868da11.js","static/chunks/188-82727dd4d2a5b9fe.js","static/chunks/pages/index-9ce520e48c3fe0ee.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
