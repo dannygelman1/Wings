06b8922e8869f8032d01a6eb3c42818f08eac3ed
Danny Gelman <dannygelman1@gmail.com>
2022-12-11 19:08:01 -0500

    hdri, reflections, and shadows

--- components/Geometry.tsx
+++ components/Geometry.tsx
@@ -2,6 +2,7 @@ import { ReactElement, useRef, useState } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { PointLight } from "three/src/lights/PointLight";
+import { BackSide, Color, DirectionalLight, DoubleSide } from "three";
 
 export const Geometry = (): ReactElement => {
   const { camera, gl, scene } = useThree();
@@ -11,8 +12,10 @@ export const Geometry = (): ReactElement => {
   controls.zoomSpeed = 0.1;
 
   const pointLight = useRef<PointLight>(null);
+  const dirLight = useRef<DirectionalLight>(null);
   scene.add(camera);
   if (pointLight.current) camera.add(pointLight.current);
+  if (dirLight.current) camera.add(dirLight.current);
 
   const [boxPosition, setBoxPosition] = useState<{
     x: number;
@@ -20,7 +23,7 @@ export const Geometry = (): ReactElement => {
     z: number;
   }>({
     x: 0,
-    y: 0,
+    y: 1,
     z: 0,
   });
 
@@ -35,13 +38,60 @@ export const Geometry = (): ReactElement => {
 
   return (
     <>
-      <pointLight ref={pointLight} position={[4, 4, 4]} />
+      <pointLight
+        ref={pointLight}
+        position={[0, 0.2, 4]}
+        intensity={0.1}
+        color={new Color(90, 60, 0)}
+      />
+      <directionalLight
+        ref={dirLight}
+        position={[0, 3, 0]}
+        intensity={0.5}
+        castShadow={true}
+      />
       <mesh
         position={[boxPosition.x, boxPosition.y, boxPosition.z]}
-        // rotation={[boxPosition.x, boxPosition.y, boxPosition.z]}
+        receiveShadow={true}
+        castShadow={true}
       >
         <boxGeometry />
         <meshStandardMaterial color="hotpink" />
+      </mesh>
+      <mesh
+        position={[boxPosition.x, boxPosition.y + 1, boxPosition.z + 2]}
+        receiveShadow={true}
+        castShadow={true}
+        scale={[2, 0.5, 4]}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" roughness={0.1} />
+      </mesh>
+      <mesh
+        position={[boxPosition.x, boxPosition.y + 2, boxPosition.z + 3]}
+        receiveShadow={true}
+        castShadow={true}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="gray" />
+      </mesh>
+      <mesh
+        position={[-4, 1, 0]}
+        receiveShadow={true}
+        castShadow={true}
+        scale={2}
+      >
+        <sphereGeometry />
+        <meshPhysicalMaterial color="gray" roughness={0.1} metalness={0.5} />
+      </mesh>
+      <mesh
+        position={[0, 0, 0]}
+        scale={10}
+        rotation={[Math.PI / 2, 0, 0]}
+        receiveShadow={true}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="blue" side={DoubleSide} />
       </mesh>
     </>
   );
--- components/PageCanvas.tsx
+++ components/PageCanvas.tsx
@@ -1,15 +1,26 @@
 import { ReactElement } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Geometry } from "./Geometry";
+import { AxesHelper } from "three";
+import { useEnvironment, Environment } from "@react-three/drei";
 
 export const PageCanvas = (): ReactElement => {
+  const map = useEnvironment({
+    files:
+      process.env.NEXT_PUBLIC_ENVIRONMENT === "Development"
+        ? `${process.env.NEXT_PUBLIC_HOST}/clarens_night_02_4k.hdr`
+        : `https://dannygelman1.github.io/Wings/clarens_night_02_4k.hdr`,
+  });
   return (
     <div className="w-full h-full h-grow bg-black absolute">
       <Canvas
-        camera={{ fov: 50, position: [1, 5, 5] }}
+        shadows={true}
+        camera={{ fov: 50, position: [-10, 0, 0] }}
         className="bg-green-500"
       >
+        <Environment map={map} background />
         <Geometry />
+        <primitive object={new AxesHelper(10)} />
       </Canvas>
     </div>
   );
