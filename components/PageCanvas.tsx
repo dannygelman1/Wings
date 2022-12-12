b3aa6037ee649d52da07df026e7a816152ca7321
Danny Gelman <dannygelman1@gmail.com>
2022-12-12 13:39:10 -0500

    bringing back env

--- components/PageCanvas.tsx
+++ components/PageCanvas.tsx
@@ -5,16 +5,16 @@ import { AxesHelper } from "three";
 import { useEnvironment, Environment } from "@react-three/drei";
 
 export const PageCanvas = (): ReactElement => {
-  // const map = useEnvironment({
-  //   files:
-  //     process.env.NEXT_PUBLIC_ENVIRONMENT === "Development"
-  //       ? `${process.env.NEXT_PUBLIC_HOST}/clarens_night_02_4k.hdr`
-  //       : "https://dannygelman1.github.io/Wings/clarens_night_02_4k.hdr",
-  // });
+  const map = useEnvironment({
+    files:
+      process.env.NEXT_PUBLIC_ENVIRONMENT === "Development"
+        ? `${process.env.NEXT_PUBLIC_HOST}/clarens_night_02_4k.hdr`
+        : "https://dannygelman1.github.io/Wings/clarens_night_02_4k.hdr",
+  });
   return (
     <div className="w-full h-full h-grow bg-black absolute">
       <Canvas shadows={true} camera={{ fov: 50, position: [-10, 0, 0] }}>
-        {/* <Environment map={map} background /> */}
+        <Environment map={map} background />
         <Geometry />
         <primitive object={new AxesHelper(10)} />
       </Canvas>
