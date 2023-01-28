9a4cccd73494a96156019f2471e8fc0c0eabd570
dannygelman1 <45411340+dannygelman1@users.noreply.github.com>
2023-01-28 04:27:06 +0000

    Deploying to gh-pages from @ dannygelman1/Wings@3a27e1e1aece699c6ace64969d1a221f06faa56c 🚀

--- _next/static/Ow58XvB7KIMwfGNn8pyXt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-2fe8122be868da11.js","static/chunks/519-4d8f3b3328908473.js","static/chunks/pages/index-d79828d654c9d1cb.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/m4TKXHQG78ZeKpHrEiBor/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-2fe8122be868da11.js","static/chunks/519-4d8f3b3328908473.js","static/chunks/pages/index-09b13e37e584bd0d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
